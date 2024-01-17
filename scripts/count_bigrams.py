b18cbd56c4c7faf6fc70d1064eeaffbf1d77eb7f
Lucia Quirke <luciarosequirke@gmail.com>
2024-01-17 23:41:04 +0000

    Save file

--- scripts/count_bigrams.py
+++ scripts/count_bigrams.py
@@ -1,4 +1,7 @@
+import io
+
 import numpy as np
+import zstandard as zstd
 from transformers import AutoTokenizer
 
 
@@ -16,8 +19,14 @@ def main():
     tokens = np.memmap(
         "/mnt/ssd-1/pile_preshuffled/standard/document.bin", dtype=np.uint16, mode="r"
     )
-    print(len(tokenizer.vocab))
-    count_bigrams(tokens, len(tokenizer.vocab))
+    counts = count_bigrams(tokens, len(tokenizer.vocab))
+
+    cctx = zstd.ZstdCompressor()
+    buffer = io.BytesIO()
+    np.save(buffer, counts)
+    with open("2-grams.zst", "wb") as compressed_file:
+        compressed_data = cctx.compress(buffer.getvalue())
+        compressed_file.write(compressed_data)
 
 
 if __name__ == "__main__":

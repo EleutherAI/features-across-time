c56a5b6d50758af23449b367fe872c00f8359b8d
Lucia Quirke <luciarosequirke@gmail.com>
2024-01-20 05:23:45 +0000

    Convert losses to BPB

--- scripts/plot_steps.py
+++ scripts/plot_steps.py
@@ -16,12 +16,11 @@ def base_2_log_ticks(values):
 def adjust_confidence_intervals(
     df, mean_col, conf_bottom_col, conf_top_col, num_items=2048
 ):
-    adjustment_factor = 1 / np.sqrt(num_items)
-    df[conf_top_col] = (
-        df[mean_col] + (df[conf_top_col] - df[mean_col]) * adjustment_factor
+    df[conf_top_col] = df[mean_col] + (df[conf_top_col] - df[mean_col]) / np.sqrt(
+        num_items
     )
-    df[conf_bottom_col] = (
-        df[mean_col] - (df[mean_col] - df[conf_bottom_col]) * adjustment_factor
+    df[conf_bottom_col] = df[mean_col] - (df[mean_col] - df[conf_bottom_col]) / np.sqrt(
+        num_items
     )
     return df
 
@@ -86,34 +85,36 @@ def plot_shuffled_loss():
     fig.write_image(Path.cwd() / "images" / "mean_shuffled_bpb.png")
 
 
-def plot_bigram_model():
-    with open(Path.cwd() / "output" / "checkpoint_bigrams_model.pkl", "rb") as f:
-        bigram_df = pickle.load(f)
+def plot_ngram_model():
+    with open(Path.cwd() / "output" / "checkpoint_ngrams_model.pkl", "rb") as f:
+        ngram_df = pickle.load(f)
 
-    df = bigram_df.groupby("step").mean().reset_index()
-    df.columns = ["step", "index", "mean_loss", "mean_conf_bottom", "mean_conf_top"]
-    df["mean_bpb"] = df["mean_loss"] * 0.3366084909549386 / math.log(2)
-    df["mean_bpb_conf_bottom"] = (
-        df["mean_conf_bottom"] * 0.3366084909549386 / math.log(2)
+    df = ngram_df.groupby("step").mean().reset_index()
+    df["mean_bigram_bpb"] = df["mean_bigram_loss"] * 0.3366084909549386 / math.log(2)
+    df["mean_bigram_bpb_conf_bottom"] = (
+        df["bottom_conf_bigram_loss"] * 0.3366084909549386 / math.log(2)
     )
-    df["mean_bpb_conf_top"] = df["mean_conf_top"] * 0.3366084909549386 / math.log(2)
+    df["mean_bigram_bpb_conf_top"] = (
+        df["top_conf_bigram_loss"] * 0.3366084909549386 / math.log(2)
+    )
 
     df = adjust_confidence_intervals(
-        df, "mean_bpb", "mean_bpb_conf_bottom", "mean_bpb_conf_top"
+        df, "mean_bigram_bpb", "mean_bigram_bpb_conf_bottom", "mean_bigram_bpb_conf_top"
     )
 
-    fig = px.line(df, x="step", y="mean_bpb")
+    fig = px.line(df, x="step", y="mean_bigram_bpb")
     fig.update_layout(
         {
-            "title": "Mean loss on bigram sequences over training steps.",
-            "yaxis_title": "Mean loss",
+            "title": "Mean BPB on bigram sequences over training steps.",
+            "yaxis_title": "Bits per byte",
             "xaxis_title": "Training step (1 step = 2,097,152 tokens)",
+            "yaxis_range": [3, 6],
         }
     )
     fig.add_traces(
         go.Scatter(
             x=df["step"],
-            y=df["mean_bpb_conf_top"],
+            y=df["mean_bigram_bpb_conf_top"],
             fill=None,
             mode="lines",
             line=dict(color="powderblue"),
@@ -123,7 +124,7 @@ def plot_bigram_model():
     fig.add_traces(
         go.Scatter(
             x=df["step"],
-            y=df["mean_bpb_conf_bottom"],
+            y=df["mean_bigram_bpb_conf_bottom"],
             fill="tonexty",
             mode="lines",
             line=dict(color="powderblue"),
@@ -135,8 +136,58 @@ def plot_bigram_model():
     fig.update_xaxes(type="log", tickvals=tick_values, ticktext=tick_texts)
     fig.write_image(Path.cwd() / "images" / "bigram_data_bpb.png")
 
+    df = ngram_df.groupby("step").mean().reset_index()
+    df["mean_unigram_bpb"] = df["mean_unigram_loss"] * 0.3366084909549386 / math.log(2)
+    df["mean_unigram_bpb_conf_bottom"] = (
+        df["bottom_conf_unigram_loss"] * 0.3366084909549386 / math.log(2)
+    )
+    df["mean_unigram_bpb_conf_top"] = (
+        df["top_conf_unigram_loss"] * 0.3366084909549386 / math.log(2)
+    )
 
-def plot_bigram_divs():
+    df = adjust_confidence_intervals(
+        df,
+        "mean_unigram_bpb",
+        "mean_unigram_bpb_conf_bottom",
+        "mean_unigram_bpb_conf_top",
+    )
+
+    fig = px.line(df, x="step", y="mean_unigram_bpb")
+    fig.update_layout(
+        {
+            "title": "BPB on unigram sequences over training steps.",
+            "yaxis_title": "Bits per byte",
+            "xaxis_title": "Training step (1 step = 2,097,152 tokens)",
+            "yaxis_range": [3, 6],
+        }
+    )
+    fig.add_traces(
+        go.Scatter(
+            x=df["step"],
+            y=df["mean_unigram_bpb_conf_top"],
+            fill=None,
+            mode="lines",
+            line=dict(color="powderblue"),
+            showlegend=False,
+        )
+    )
+    fig.add_traces(
+        go.Scatter(
+            x=df["step"],
+            y=df["mean_unigram_bpb_conf_bottom"],
+            fill="tonexty",
+            mode="lines",
+            line=dict(color="powderblue"),
+            showlegend=False,
+        )
+    )
+
+    tick_values, tick_texts = base_2_log_ticks(df["step"])
+    fig.update_xaxes(type="log", tickvals=tick_values, ticktext=tick_texts)
+    fig.write_image(Path.cwd() / "images" / "unigram_data_bpb.png")
+
+
+def plot_divs():
     with open(Path.cwd() / "output" / "checkpoint_bigrams.pkl", "rb") as f:
         bigram_df = pickle.load(f)
 
@@ -191,8 +242,8 @@ def plot_bigram_divs():
 
 
 def main():
-    plot_bigram_model()
-    plot_bigram_divs()
+    plot_ngram_model()
+    plot_divs()
     plot_shuffled_loss()
 
 

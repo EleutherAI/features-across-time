895bcd7d8a0966fdf8628225dfe7c545cb6c7255
Lucia Quirke <luciarosequirke@gmail.com>
2024-01-23 03:38:04 +0000

    update ngram sampling to cover all pythia models

--- scripts/ngram_samples.py
+++ scripts/ngram_samples.py
@@ -1,6 +1,7 @@
 import math
 import pickle
 from pathlib import Path
+import argparse
 
 import numpy as np
 import pandas as pd
@@ -23,12 +24,12 @@ class NgramModel:
             bigram_counts = pickle.load(f)
 
         self.unigrams = (
-            torch.tensor(bigram_counts.toarray(), dtype=torch.float32).sum(dim=1).cuda()
+            torch.tensor(bigram_counts.toarray().astype(np.float32)).sum(dim=1).cuda()
         )
         self.bigrams = torch.sparse_csr_tensor(
-            bigram_counts.indptr,
-            bigram_counts.indices,
-            bigram_counts.data,
+            bigram_counts.indptr.astype(np.int64),
+            bigram_counts.indices.astype(np.int64),
+            bigram_counts.data.astype(np.float32),
             dtype=torch.float32,
             device="cuda",
         )
@@ -101,38 +102,38 @@ def ngram_model_worker(
 
         running_step_unigram_loss_mean = 0.0
         running_step_bigram_loss_mean = 0.0
-        weight = num_samples // batch
-        for _ in range(num_samples // batch):
+        num_iters = math.ceil(num_samples / batch)
+        for _ in range(num_iters):
             unigram_sample = ngram_model.generate_unigrams()
             unigram_outputs = model(unigram_sample)
-            unigram_loss_sum = F.cross_entropy(
+            unigram_loss_mean = F.cross_entropy(
                 unigram_outputs.logits[:, :-1].flatten(0, 1),
                 unigram_sample[:, 1:].flatten(),
                 reduction="mean",
             ).item()
-            running_step_unigram_loss_mean += unigram_loss_sum / weight
+            running_step_unigram_loss_mean += (unigram_loss_mean / num_iters)
             del unigram_sample, unigram_outputs
 
             bigram_sample = ngram_model.generate_bigrams()
             bigram_outputs = model(bigram_sample)
-            bigram_loss_sum = F.cross_entropy(
+            bigram_loss_mean = F.cross_entropy(
                 bigram_outputs.logits[:, :-1].flatten(0, 1),
                 bigram_sample[:, 1:].flatten(),
                 reduction="mean",
             ).item()
-            running_step_bigram_loss_mean += bigram_loss_sum / weight
+            running_step_bigram_loss_mean += (bigram_loss_mean / num_iters)
             del bigram_sample, bigram_outputs
 
         unigram_means.append(running_step_unigram_loss_mean)
         unigram_conf_intervals.append(
             get_confidence_intervals(
-                running_step_unigram_loss_mean, (num_samples // batch) * 2048
+                running_step_unigram_loss_mean, num_iters * batch * 2048
             )
         )
         bigram_means.append(running_step_bigram_loss_mean)
         bigram_conf_intervals.append(
             get_confidence_intervals(
-                running_step_bigram_loss_mean, (num_samples // batch) * 2048
+                running_step_bigram_loss_mean, num_iters * batch * 2048
             )
         )
 
@@ -155,39 +156,58 @@ def ngram_model_worker(
     )
 
 
-def main():
-    model_name = "EleutherAI/pythia-410m"
-    ngram_path = "/mnt/ssd-1/lucia/pythia-deduped-bigrams.pkl"
+def main(ngram_path: str):
+    model_batch_sizes = {
+        "pythia-14m": 16,
+        "pythia-70m": 16,
+        "pythia-160m": 16,
+        "pythia-410m": 8,
+        "pythia-1b": 2,
+        "pythia-1.4b": 2,
+        "pythia-2.8b": 2,
+        "pythia-6.9b": 2,   
+        "pythia-12b": 2,   
+    }
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
     d_vocab = len(tokenizer.vocab)
     num_samples = 32
-    batch = 2
 
     log_steps = [0] + [2**i for i in range(int(math.log2(512)) + 1)]
     linear_steps = list(range(1000, 144000, 1000))
     steps = log_steps + linear_steps
 
     num_gpus = torch.cuda.device_count()
+    mp.set_start_method("spawn")
+
     max_steps_per_chunk = math.ceil(len(steps) / num_gpus)
     step_indices = [
         steps[i : i + max_steps_per_chunk]
         for i in range(0, len(steps), max_steps_per_chunk)
     ]
-    args = [
-        (i, step_indices[i], model_name, ngram_path, num_samples, batch, d_vocab)
-        for i in range(num_gpus)
-    ]
 
-    print(f"Parallelising over {num_gpus} GPUs...")
-    mp.set_start_method("spawn")
-    with mp.Pool(num_gpus) as pool:
-        dfs = pool.starmap(ngram_model_worker, args)
+    for model_name, batch in model_batch_sizes.items():
+        args = [
+            (i, step_indices[i], f'EleutherAI/{model_name}', ngram_path, num_samples, batch, d_vocab)
+            for i in range(num_gpus)
+        ]
+        print(f"Parallelising over {num_gpus} GPUs...")
+        with mp.Pool(num_gpus) as pool:
+            dfs = pool.starmap(ngram_model_worker, args)
 
-    with open(Path.cwd() / "output" / "step_ngrams_model_means.pkl", "wb") as f:
-        pickle.dump(pd.concat(dfs), f)
+        with open(Path.cwd() / "output" / f"step_ngrams_model_means_{model_name}.pkl", "wb") as f:
+            pickle.dump(pd.concat(dfs), f)
 
     plot_ngram_model_bpb()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--ngram_path",
+        default="/mnt/ssd-1/lucia/pythia-deduped-bigrams.pkl",
+        help="Path to pickled sparse scipy array of bigram counts over the Pile",
+    )
+    args = parser.parse_args()
+    main(args.ngram_path)

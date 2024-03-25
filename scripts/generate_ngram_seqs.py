fa13e39bfbdbf7ef3a0a5cf205c61744f05ee955
Lucia Quirke <luciarosequirke@gmail.com>
2024-03-25 04:57:20 +0000

    Add trigram sampling code

--- scripts/generate_ngram_seqs.py
+++ scripts/generate_ngram_seqs.py
@@ -1,4 +1,5 @@
 import pickle
+import time
 
 import numpy as np
 import torch
@@ -7,6 +8,7 @@ from numpy.typing import NDArray
 from scipy.stats import entropy
 from tqdm.auto import tqdm
 from transformers import AutoTokenizer
+from tokengrams import InMemoryIndex, MemmapIndex
 
 
 class NgramModel:
@@ -26,10 +28,16 @@ class NgramModel:
 
         self.prob_matrix = self.bigrams / self.bigrams.sum(axis=1)[:, None]
 
+
+        one_billion_tokens_path = "/mnt/ssd-1/nora/pile-head.bin"
+        # ten_billion_tokens_path = "/mnt/ssd-1/nora/pile-10g.bin"
+
+        one_billion_tokens_index_path = "/mnt/ssd-1/nora/pile-head.idx"
+        self.mmap_index = MemmapIndex(one_billion_tokens_path, one_billion_tokens_index_path)
+
     def generate_bigrams(self) -> torch.Tensor:
         """Auto-regressively generate bigram model sequence. Initialize each
         sequence by sampling from a unigram model."""
-        # TODO expand unigrams to avoid unsqueeze
         result = [
             torch.multinomial(self.unigrams, self.batch, replacement=True).unsqueeze(1)
         ]
@@ -39,6 +47,29 @@ class NgramModel:
             next = torch.multinomial(torch.tensor(rows, device="cuda"), 1)
             result.append(next)
         return torch.cat(result, dim=1)
+
+    def generate_trigrams(self) -> torch.Tensor:
+        """Auto-regressively generate trigram model sequence. Initialize each
+        sequence by sampling from a unigram and then a bigram model."""
+        start = time.time()
+        result = torch.zeros(self.batch, self.seq_len, dtype=torch.int64)
+        result[:, 0] = torch.multinomial(self.unigrams, self.batch, replacement=True)
+        rows = self.bigrams[result[:, 0].cpu().numpy()]
+        result[:, 1] = torch.multinomial(torch.tensor(rows, device="cuda"), 1).squeeze(-1)
+
+        # Build as lists
+        for i in tqdm(range(2, self.seq_len)):
+            next = torch.zeros(self.batch, dtype=torch.int64)
+            for j in range(self.batch): # todo vectorized mmap_index.sample over batch
+                item = result[j, i - 2:i].tolist() # last two tokens of each batch [batch, 2]
+                # todo try catch if we hit a sequence that only ever exists at the end of lines
+                # the index is currently built on unchunked data so this shouldn't happen
+                next[j] = self.mmap_index.sample(item)
+            result[:, i] = next
+        # print(result.shape, "time taken: ", time.time() - start)
+
+        return result
+
 
     def perplexity(self, sample: NDArray):
         nll = -np.log(
@@ -64,12 +95,7 @@ def conditional_entropy(arr: NDArray):
     print("Entropy (bpb):", H * bpb_ratio)
 
 
-def main():
-    bigrams_path = "pythia-deduped-bigrams.pkl"
-    batch = 4
-    ngram_model = NgramModel(bigrams_path, batch)  # /mnt/ssd-1/lucia/
-
-    def generate_samples():
+def generate_bigram_samples(batch, ngram_model):
         data = np.zeros((1024, 2049), dtype=np.int64)
         for i in tqdm(range(0, 1024, batch)):
             sample = ngram_model.generate_bigrams()
@@ -77,12 +103,24 @@ def main():
 
         np.save("bigram-sequences.npy", data)
 
-    generate_samples()
+def generate_trigram_samples(batch, ngram_model):
+        data = np.zeros((1024, 2049), dtype=np.int64)
+        for i in tqdm(range(0, 1024, batch)):
+            sample = ngram_model.generate_trigrams()
+            data[i : i + batch, :] = sample.cpu().numpy()
+
+        np.save("trigram-sequences.npy", data)
+
+def main():
+    bigrams_path = "pythia-deduped-bigrams.pkl"
+    batch = 4
+    ngram_model = NgramModel(bigrams_path, batch)  # /mnt/ssd-1/lucia/
+
+    # generate_bigram_samples(batch, ngram_model)
+    generate_trigram_samples(batch, ngram_model)
 
     # data = np.load('bigram-sequences.npy')
-    # print(data[1022, :].shape)
     # for row in data:
-    # breakpoint()
     # ngram_model.cross_entropy(data[0])
 
     # with open(bigrams_path, "rb") as f:

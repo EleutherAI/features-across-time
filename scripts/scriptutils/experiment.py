325c99683b689e68900078deebfb068dfa636226
Lucia Quirke <luciarosequirke@gmail.com>
2024-03-28 04:11:24 +0000

    Add support for running experiments on a subset of GPUs

--- scripts/scriptutils/experiment.py
+++ scripts/scriptutils/experiment.py
@@ -28,10 +28,14 @@ def run_experiment_workers(
     worker: Callable[[int, Experiment, str, str, str, list[str]], pd.DataFrame],
     ngram_path: str, 
     pile_path: str, 
-    tmp_cache_path: str
+    tmp_cache_path: str,
+    gpu_ids: list[int] = None
 ):
-    num_gpus = torch.cuda.device_count()
-    max_steps_per_chunk = math.ceil(len(experiment.steps) / num_gpus)
+    if not gpu_ids:
+        gpu_ids = list(range(torch.cuda.device_count()))
+
+    max_steps_per_chunk = math.ceil(len(experiment.steps) / len(gpu_ids))
+    
     step_indices = [
         experiment.steps[i : i + max_steps_per_chunk]
         for i in range(0, len(experiment.steps), max_steps_per_chunk)
@@ -40,17 +44,17 @@ def run_experiment_workers(
 
     args = [
         (
-            i,
+            gpu_id,
             experiment,
             ngram_path,
             pile_path,
             tmp_cache_path,
             step_indices[i],
         )
-        for i in range(len(step_indices))
+        for i, gpu_id in enumerate(gpu_ids)
     ]
     print(f"Parallelising over {len(step_indices)} GPUs...")
-    with mp.Pool(len(step_indices)) as pool:
+    with mp.Pool(len(gpu_ids)) as pool:
         dfs = pool.starmap(worker, args)
 
     return pd.concat(dfs)

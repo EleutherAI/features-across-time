53f2fef4349dc516096a95f6dd433b7dab7a4540
Lucia Quirke <luciarosequirke@gmail.com>
2024-05-22 06:23:14 +0000

    refactor vision plots

--- scripts/plots/plot_vision.py
+++ scripts/plots/plot_vision.py
@@ -29,7 +29,7 @@ marker_series = [
     "hexagram",
 ]
 
-def get_num_classes(dataset_str: str):
+def num_classes(dataset_str: str):
     ds = load_from_disk(f'/mnt/ssd-1/lucia/vision-data/max-entropy-{dataset_str}.hf')
     ds.set_format('torch', columns=['pixel_values','label'])
     return len(torch.unique(ds['label']))
@@ -53,7 +53,7 @@ def main(debug: bool):
         ]
         data_path = Path('/') / 'mnt' / 'ssd-1' / 'lucia' / 'features-across-time' / '24-05-22'
 
-        grayscale = "-greyscale" if (dataset_str == "mnist" or dataset_str == "fashion_mnist") else ""
+        grayscale = "-grayscale" if (dataset_str == "mnist" or dataset_str == "fashion_mnist") else ""
         df = pd.read_csv(data_path / f'vision-{dataset_str}{grayscale}.csv')
 
         if dataset_str == "svhn:cropped_digits":
@@ -78,31 +78,26 @@ def plot(
 ):
     if not debug:
         write_garbage()
+
     tick_values, tick_texts = base_2_log_ticks(df["step"], step=2)
+    uniform_random_loss = math.log(num_classes(dataset_str))
+    uniform_random_accuracy = (1 / num_classes(dataset_str))
 
-    num_classes = get_num_classes(dataset_str)
-    uniform_random_loss = math.log(num_classes)
-
-    # loss_columns = [name for name in df.columns if name.endswith('_loss')]
-    # accuracy_columns = [name for name in df.columns if name.endswith('_accuracy')]
-    # interventions = [col.split('_loss')[0] for col in df.columns if col.endswith('_loss')]
     ot_col_names = {
         "cqn": "1st order (CQN)",
         "shifted": "1st order (hypercube)",
         "got": "2nd order (Gaussian OT)",
-        # "real": "Val. set",
+        "real": "Val. set",
     }
     maxent_col_names = {
         "independent": "1st order (ICS)",
         "maxent": "1st order (hypercube)",
         "gaussian": "2nd order (Gaussian)",
-        # "real": "Val. set",
+        # "truncated_normal": "2nd order (truncated normal)",
+        "real": "Val. set",
     }
 
     for col_dict in [ot_col_names, maxent_col_names]:
-        for col in col_dict.keys():
-            df[f'{col}_loss'] = df[f'{col}_loss']
-
         if "maxent" in col_dict:
             title = f"Max-entropy sampling across time ({pretty_dataset_str})"
         else:
@@ -121,8 +116,10 @@ def plot(
             model_df = df[df["net"] == model]
             
             for j, (col_name, legend_name) in enumerate(col_dict.items()):
-                color = px.colors.qualitative.Plotly[j]
-                mean_loss_per_step = model_df.groupby("step")[f"{col_name}_loss"].mean()
+                opaque_color = px.colors.qualitative.Plotly[j] if col_name != "real" else "#000000"
+                transparent_color = hex_to_rgba(opaque_color, opacity=0.17)
+
+                # Add mean line over model architectures
                 mean_acc_per_step = model_df.groupby("step")[f"{col_name}_accuracy"].mean()
                 fig.add_trace(
                     go.Scatter(
@@ -131,12 +128,14 @@ def plot(
                         mode="lines+markers",
                         marker=dict(size=5, symbol=marker_series[0]),
                         name=legend_name,
-                        line=dict(color=color),
+                        line=dict(color=opaque_color),
                         showlegend=i==df["net"].nunique() - 1,
                     ),
                     row=1,
                     col=i + 1,
                 )
+
+                mean_loss_per_step = model_df.groupby("step")[f"{col_name}_loss"].mean()
                 fig.add_trace(
                     go.Scatter(
                         x=model_df[model_df["arch"] == model_df["arch"].unique()[0]]["step"],
@@ -144,17 +143,16 @@ def plot(
                         mode="lines+markers",
                         marker=dict(size=5, symbol=marker_series[0]),
                         name=legend_name,
-                        line=dict(color=color),
+                        line=dict(color=opaque_color),
                         showlegend=False,
                     ),
                     row=2,
                     col=i + 1,
                 )
             
+                # Add lines for individual model architectures
                 for arch in model_df["arch"].unique():
                     arch_df = model_df[model_df["arch"] == arch]
-
-                    transparent_color = hex_to_rgba(color, opacity=0.17)
 
                     fig.add_trace(
                         go.Scatter(
@@ -181,14 +179,15 @@ def plot(
                         col=i + 1,
                     )
                 
+            # Indicate baseline loss and accuracies
             fig.add_shape(
                 row=1,
                 col=i + 1,
                 type="line",
                 x0=1,
-                y0=0.1,
+                y0=uniform_random_accuracy,
                 x1=2**16,
-                y1=0.1,
+                y1=uniform_random_accuracy,
                 line=dict(color="black", width=2, dash="dash"),
             )
             fig.add_shape(
@@ -201,65 +200,9 @@ def plot(
                 y1=uniform_random_loss,
                 line=dict(color="black", width=2, dash="dash"),
             )
-                
-            mean_loss_per_step = model_df.groupby("step")[f"real_loss"].mean()
-            mean_acc_per_step = model_df.groupby("step")[f"real_accuracy"].mean()
-            fig.add_trace(
-                go.Scatter(
-                    x=arch_df["step"],
-                    y=mean_acc_per_step,
-                    mode="lines+markers",
-                    marker=dict(size=5, symbol=marker_series[0]),
-                    name="Val. set",
-                    line=dict(color="black"),
-                    showlegend=i==df["net"].nunique() - 1,
-                ),
-                row=1,
-                col=i + 1,
-            )
-            fig.add_trace(
-                go.Scatter(
-                    x=arch_df["step"],
-                    y=mean_loss_per_step,
-                    mode="lines+markers",
-                    marker=dict(size=5, symbol=marker_series[0]),
-                    line=dict(color="black"),
-                    showlegend=False,
-                ),
-                row=2,
-                col=i + 1,
-            )
-            for arch in model_df["arch"].unique():
-                arch_df = model_df[model_df["arch"] == arch]
-
-                transparent_color = hex_to_rgba("#000000", opacity=0.17)
-
-                fig.add_trace(
-                    go.Scatter(
-                        x=arch_df["step"],
-                        y=arch_df[f"real_accuracy"],
-                        mode="lines",
-                        name=arch,
-                        line=dict(color=transparent_color),
-                        showlegend=False,
-                    ),
-                    row=1,
-                    col=i + 1,
-                )
-                fig.add_trace(
-                    go.Scatter(
-                        x=arch_df["step"],
-                        y=arch_df[f"real_loss"],
-                        mode="lines",
-                        name=arch,
-                        line=dict(color=transparent_color),
-                        showlegend=False,
-                    ),
-                    row=2,
-                    col=i + 1,
-                )
 
         fig.update_layout(
+            title=title, 
             width=1150,
             height=600,
             legend=dict(
@@ -270,24 +213,25 @@ def plot(
                 font=dict(size=8),
                 bgcolor="rgba(255, 255, 255, 0.85)",
             ),
-            margin=dict(l=20, r=20, t=50, b=60)
+            margin=dict(l=20, r=20, t=50, b=60),
         )
-        fig.update_layout(title=title, margin=dict(t=80))
         fig.update_xaxes(
             title_text="", type="log", tickvals=tick_values, ticktext=tick_texts
         )
-        fig.update_yaxes(
-            row=2, col=1, title_text="Loss", title_font=dict(size=10), title_standoff=10
-        )
+        
         y_ticks = np.linspace(0, 1, num=6)
         ticktext = ["{:.0%}".format(tick) for tick in y_ticks]
         ticktext[0], ticktext[-1] = "", ""
+        fig.update_yaxes(row=1, range=[0, 1])
         fig.update_yaxes(
-            tickvals=y_ticks, ticktext=ticktext,
-            row=1, col=1, title_text="Accuracy", tickformat=".0%", title_font=dict(size=10), title_standoff=10
+            row=1, col=1, tickvals=y_ticks, ticktext=ticktext, title_text="Accuracy", tickformat=".0%", title_font=dict(size=10), title_standoff=10
         )
-        fig.update_yaxes(range=[0, 1], row=1)
-        fig.update_yaxes(range=[0, 10], row=2)
+
+        fig.update_yaxes(row=2, range=[0, 10])
+        fig.update_yaxes(
+            row=2, col=1, title_text="Loss", title_font=dict(size=10), title_standoff=10
+        )
+        
         fig.add_annotation(
             dict(
                 text="Training steps",

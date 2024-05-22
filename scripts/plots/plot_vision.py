6065bec9eda0cc8e1e521ffa9b155d34ed3db757
Lucia Quirke <luciarosequirke@gmail.com>
2024-05-22 07:36:19 +0000

    refactor plot vision

--- scripts/plots/plot_vision.py
+++ scripts/plots/plot_vision.py
@@ -30,15 +30,14 @@ marker_series = [
 ]
 
 def num_classes(dataset_str: str):
-    ds = load_from_disk(f'/mnt/ssd-1/lucia/vision-data/max-entropy-{dataset_str}.hf')
+    ds = load_from_disk(Path.cwd() / 'vision-data' / f'dury-{dataset_str}.hf')
     ds.set_format('torch', columns=['pixel_values','label'])
     return len(torch.unique(ds['label']))
 
 
-def main(debug: bool):
-    os.makedirs(Path.cwd() / "images", exist_ok=True)
+def main(debug: bool, data_path = Path.cwd() / '24-05-22', images_path = Path.cwd() / 'images'):
+    os.makedirs(images_path, exist_ok=True)
     
-
     for dataset_str, pretty_dataset_str in [
         ("cifar10", "CIFAR10"), 
         # ("cifarnet", "CIFAR-Net"), 
@@ -51,8 +50,6 @@ def main(debug: bool):
             "regnet", "RegNet-Y",
             "swin", "Swin Transformer",
         ]
-        data_path = Path('/') / 'mnt' / 'ssd-1' / 'lucia' / 'features-across-time' / '24-05-22'
-
         grayscale = "-grayscale" if (dataset_str == "mnist" or dataset_str == "fashion_mnist") else ""
         df = pd.read_csv(data_path / f'vision-{dataset_str}{grayscale}.csv')
 
@@ -67,14 +64,14 @@ def main(debug: bool):
             df.loc[condition, df.columns] = df_svhn_swin.loc[condition_svhn_swin, df.columns].values
             df.to_csv('tmp.csv')
 
-        df["pretty_model_name"] = df["net"].map(dict(zip(model_metadata[::2], model_metadata[1::2])))
+        df["pretty_net"] = df["net"].map(dict(zip(model_metadata[::2], model_metadata[1::2])))
         df = df[df['ds'] == dataset_str]
-        image_prefix = Path.cwd() / 'images'
-        plot(dataset_str, pretty_dataset_str, df, image_prefix, debug)
+        
+        plot(dataset_str, pretty_dataset_str, df, images_path, debug)
 
 
 def plot(
-    dataset_str: str, pretty_dataset_str: str, df: pd.DataFrame, image_prefix: str, debug: bool
+    dataset_str: str, pretty_dataset_str: str, df: pd.DataFrame, images_path: str, debug: bool
 ):
     if not debug:
         write_garbage()
@@ -97,22 +94,21 @@ def plot(
         "real": "Val. set",
     }
 
-    for col_dict in [ot_col_names, maxent_col_names]:
-        if "maxent" in col_dict:
-            title = f"Max-entropy sampling across time ({pretty_dataset_str})"
-        else:
-            title = f"Optimal transport across time ({pretty_dataset_str})"
-    
+    for col_dict, intervention in [
+            (ot_col_names, "Optimal transport"), 
+            (maxent_col_names, "Max-entropy sampling")
+        ]:
+        title = f"{intervention} across time ({pretty_dataset_str})"
         fig = make_subplots(
             rows=2,
             cols=df["net"].nunique(),
             shared_xaxes=True,
             shared_yaxes=True,
-            subplot_titles=df["pretty_model_name"].unique(),
+            subplot_titles=df["pretty_net"].unique(),
             horizontal_spacing=0.02,
             vertical_spacing=0.04,
         )
-        for i, model in enumerate(df["net"].unique()):
+        for col_idx, model in enumerate(df["net"].unique()):
             model_df = df[df["net"] == model]
             
             for j, (col_name, legend_name) in enumerate(col_dict.items()):
@@ -120,69 +116,42 @@ def plot(
                 transparent_color = hex_to_rgba(opaque_color, opacity=0.17)
 
                 # Add mean line over model architectures
-                mean_acc_per_step = model_df.groupby("step")[f"{col_name}_accuracy"].mean()
-                fig.add_trace(
-                    go.Scatter(
-                        x=model_df[model_df["arch"] == model_df["arch"].unique()[0]]["step"],
-                        y=mean_acc_per_step,
-                        mode="lines+markers",
-                        marker=dict(size=5, symbol=marker_series[0]),
-                        name=legend_name,
-                        line=dict(color=opaque_color),
-                        showlegend=i==df["net"].nunique() - 1,
-                    ),
-                    row=1,
-                    col=i + 1,
-                )
-
-                mean_loss_per_step = model_df.groupby("step")[f"{col_name}_loss"].mean()
-                fig.add_trace(
-                    go.Scatter(
-                        x=model_df[model_df["arch"] == model_df["arch"].unique()[0]]["step"],
-                        y=mean_loss_per_step,
-                        mode="lines+markers",
-                        marker=dict(size=5, symbol=marker_series[0]),
-                        name=legend_name,
-                        line=dict(color=opaque_color),
-                        showlegend=False,
-                    ),
-                    row=2,
-                    col=i + 1,
-                )
-            
-                # Add lines for individual model architectures
-                for arch in model_df["arch"].unique():
-                    arch_df = model_df[model_df["arch"] == arch]
-
+                for row_idx, measure in enumerate(['accuracy', 'loss']):
+                    mean_measure_per_step = model_df.groupby("step")[f"{col_name}_{measure}"].mean()
                     fig.add_trace(
                         go.Scatter(
-                            x=arch_df["step"],
-                            y=arch_df[f"{col_name}_accuracy"],
-                            mode="lines",
-                            name=arch,
-                            line=dict(color=transparent_color),
-                            showlegend=False,
+                            x=model_df[model_df["arch"] == model_df["arch"].unique()[0]]["step"],
+                            y=mean_measure_per_step,
+                            mode="lines+markers",
+                            marker=dict(size=5, symbol=marker_series[0]),
+                            name=legend_name,
+                            line=dict(color=opaque_color),
+                            showlegend=col_idx==df["net"].nunique() - 1 and row_idx == 0,
                         ),
-                        row=1,
-                        col=i + 1,
+                        row=row_idx + 1,
+                        col=col_idx + 1,
                     )
-                    fig.add_trace(
-                        go.Scatter(
-                            x=arch_df["step"],
-                            y=arch_df[f"{col_name}_loss"],
-                            mode="lines",
-                            name=arch,
-                            line=dict(color=transparent_color),
-                            showlegend=False,
-                        ),
-                        row=2,
-                        col=i + 1,
-                    )
+
+                    for arch in model_df["arch"].unique():
+                        arch_df = model_df[model_df["arch"] == arch]
+
+                        fig.add_trace(
+                            go.Scatter(
+                                x=arch_df["step"],
+                                y=arch_df[f"{col_name}_{measure}"],
+                                mode="lines",
+                                # name=arch,
+                                line=dict(color=transparent_color),
+                                showlegend=False,
+                            ),
+                            row=row_idx + 1,
+                            col=col_idx + 1,
+                        )
                 
             # Indicate baseline loss and accuracies
             fig.add_shape(
                 row=1,
-                col=i + 1,
+                col=col_idx + 1,
                 type="line",
                 x0=1,
                 y0=uniform_random_accuracy,
@@ -192,7 +161,7 @@ def plot(
             )
             fig.add_shape(
                 row=2,
-                col=i + 1,
+                col=col_idx + 1,
                 type="line",
                 x0=1,
                 y0=uniform_random_loss,
@@ -243,7 +212,7 @@ def plot(
                 font=dict(size=14),
             )
         )
-        fig.write_image(image_prefix / f"vision-{dataset_str}-{'maxent' if 'maxent' in col_dict else 'ot'}.pdf", format="pdf")
+        fig.write_image(images_path / f"vision-{dataset_str}-{'maxent' if 'maxent' in col_dict else 'ot'}.pdf", format="pdf")
 
 
 if __name__ == "__main__":

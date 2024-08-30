f063854ead58494ee8d53dbb2cd78a7b37b865a4
Lucia Quirke <luciarosequirke@gmail.com>
2024-08-30 07:17:42 +0000

    add fresh inference script using pre-calculated dists

---
+++ scripts/inference/tokengrams_steps.py
@@ -0,0 +1,514 @@
+import math
+import os
+import time
+from argparse import ArgumentParser
+from collections import defaultdict
+from pathlib import Path
+from typing import Callable
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.multiprocessing as mp
+import torch.nn.functional as F
+from datasets import load_from_disk
+from torch.utils.data import DataLoader, Dataset
+from tqdm.auto import tqdm
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from scripts.script_utils.divergences import (
+    js_divergence_from_logits,
+    kl_divergence_from_logits,
+)
+
+
+def set_seeds(seed=42):
+    import random
+
+    random.seed(seed)  # unused
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+class NgramDistDatasetBFloat16(Dataset):
+    def __init__(self, memmap_file, num_samples, seq_len, d_vocab):
+        self.data = np.memmap(
+            memmap_file,
+            dtype=np.uint16,
+            mode="r",
+            shape=(num_samples * seq_len, d_vocab),
+        )
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+class NgramTokensDataset(Dataset):
+    def __init__(self, n, num_samples, seq_len, d_vocab):
+        num_tokens = num_samples * seq_len
+        data_path = Path("/mnt/ssd-1/lucia/ngrams-across-time/data")
+
+        tokens_path = data_path / f"{n}-gram-pile-tokens-bf16-21_shards.npy"
+        probs_path = data_path / f"{n}-gram-pile-dists-bf16-21_shards.npy"
+        logprobs_path = data_path / f"{n}-gram-pile-dists-bf16-21_shards-logprobs.npy"
+
+        if os.path.exists(tokens_path):
+            self.data = np.memmap(
+                str(tokens_path), dtype=np.uint16, mode="r+", shape=(num_tokens)
+            )
+        elif os.path.exists(probs_path):
+            self.data = np.memmap(
+                str(tokens_path), dtype=np.uint16, mode="w+", shape=(num_tokens)
+            )
+
+            ngram_probs_bfloat16 = np.memmap(
+                str(probs_path), dtype=np.uint16, mode="r", shape=(num_tokens, d_vocab)
+            )
+            for i in tqdm(
+                range(num_samples), desc=f"Sampling {n}-gram tokens from probs"
+            ):
+                seq_slice = slice(i * seq_len, (i + 1) * seq_len)
+                ngram_probs = self.bfloat16_to_float32(
+                    ngram_probs_bfloat16[seq_slice]
+                ).copy()
+                ngram_probs[ngram_probs == 0] = 1e-6
+
+                self.data[seq_slice] = (
+                    torch.multinomial(torch.from_numpy(ngram_probs), 1)
+                    .squeeze()
+                    .to(torch.uint16)
+                    .numpy()
+                )
+        elif os.path.exists(logprobs_path):
+            self.data = np.memmap(
+                str(tokens_path), dtype=np.uint16, mode="w+", shape=(num_tokens)
+            )
+
+            ngram_logprobs_bfloat16 = np.memmap(
+                str(logprobs_path),
+                dtype=np.uint16,
+                mode="r",
+                shape=(num_tokens, d_vocab),
+            )
+            for i in tqdm(
+                range(num_samples), desc=f"Sampling {n}-gram tokens from logprobs"
+            ):
+                seq_slice = slice(i * seq_len, (i + 1) * seq_len)
+                ngram_logprobs = self.bfloat16_to_float32(
+                    ngram_logprobs_bfloat16[seq_slice]
+                )
+                ngram_probs = np.exp(ngram_logprobs)
+
+                self.data[seq_slice] = (
+                    torch.multinomial(torch.from_numpy(ngram_probs), 1)
+                    .squeeze()
+                    .to(torch.uint16)
+                    .numpy()
+                )
+        else:
+            raise ValueError(
+                f"Could not find tokens, probs, or logprobs for {n}-grams.\
+                please use Tokengrams to generate"
+            )
+
+    def __len__(self):
+        return len(self.data_bfloat16)
+
+    def __getitem__(self, idx):
+        return self.data_bfloat16[idx]
+
+    def float32_to_bfloat16(self, x):
+        """Convert float32 to bfloat16 values represented as uint16."""
+        x = np.asarray(x)
+        x_view = x.view(np.uint32)
+        return (x_view >> 16).astype(np.uint16)
+
+    def bfloat16_to_float32(self, x):
+        """Convert bfloat16 values represented as uint16 to float32."""
+        x = np.asarray(x)
+        return np.frombuffer(
+            np.left_shift(x.astype(np.uint32), 16).tobytes(), dtype=np.float32
+        ).reshape(x.shape)
+
+
+def bfloat16_to_float32(x):
+    """Convert bfloat16 values represented as uint16 to float32."""
+    x = np.asarray(x)
+    return np.frombuffer(
+        np.left_shift(x.astype(np.uint32), 16).tobytes(), dtype=np.float32
+    ).reshape(x.shape)
+
+
+def collate_bfloats(batch):
+    batch = np.stack(batch)
+    batch = bfloat16_to_float32(batch)
+    return torch.from_numpy(batch)
+
+
+@torch.inference_mode()
+def worker(
+    gpu_id: int,
+    steps: list[int],
+    model_name: str,
+    batch_size: str,
+    num_samples: int,
+    d_vocab: int,
+    seq_len: int,
+    ngram_orders: list[int],
+    ngram_path: Path,
+    val_path: Path,
+    div: bool,
+    loss: bool,
+) -> pd.DataFrame:
+    print(f"Calculating loss? {loss}. Calculating divs? {div}")
+    torch.cuda.set_device(gpu_id)
+
+    # Load input data
+    val_ds = DataLoader(load_from_disk(str(val_path)), batch_size=batch_size)
+    if div:
+        val_ngram_dists = {
+            n: DataLoader(
+                NgramDistDatasetBFloat16(
+                    str(ngram_path / f"{n}-gram-pile-dists-bf16-21_shards.npy"),
+                    num_samples,
+                    seq_len,
+                    d_vocab,
+                ),
+                collate_fn=collate_bfloats,
+                batch_size=batch_size * seq_len,
+            )
+            for n in ngram_orders
+        }
+    if loss:
+        print("Loading or generating ngram tokens...")
+        val_ngram_tokens = {
+            n: DataLoader(
+                NgramTokensDataset(n, num_samples, seq_len, d_vocab),
+                batch_size=batch_size * seq_len,
+            )
+            for n in ngram_orders
+        }
+    print("Loaded data...")
+
+    # Collect inference data
+    data = defaultdict(list)
+    data["step"] = steps
+
+    num_iters = math.ceil(num_samples / batch_size)
+    pbar = tqdm(total=len(steps) * num_iters, position=gpu_id)
+    for step in steps:
+        success = False
+        for retry in range(3):
+            try:
+                model = AutoModelForCausalLM.from_pretrained(
+                    f"EleutherAI/{model_name}",
+                    torch_dtype=torch.float16,
+                    revision=f"step{step}",
+                    cache_dir=".cache",
+                    # quantization_config=BitsAndBytesConfig(load_in_4bit=True)
+                ).cuda()
+                success = True
+                break
+            except Exception as e:
+                if retry < 2:
+                    print(f"Attempt {retry + 1} failed, retrying in 2 seconds...", e)
+                    time.sleep(2)
+                else:
+                    print("Failed to retrieve model at step", step, e)
+                    for n in ngram_orders:
+                        if loss:
+                            data[f"mean_{n}_gram_loss"].append(np.nan)
+                        if div:
+                            data[f"mean_{n}_gram_kl_div"].append(np.nan)
+                            data[f"mean_{n}_gram_js_div"].append(np.nan)
+        if not success:
+            continue
+
+        val = iter(val_ds)
+        ngram_dist_dls = {n: iter(val_ngram_dists[n]) for n in ngram_orders}
+        ngram_token_dls = {n: iter(val_ngram_tokens[n]) for n in ngram_orders}
+
+        running_means = defaultdict(float)
+        for _ in range(num_iters):
+            tokens = next(val)["input_ids"].cuda()
+            logits = model(tokens).logits[:, :, :d_vocab].flatten(0, 1)
+
+            for n in ngram_orders:
+                if div:
+                    ngram_logprobs = next(ngram_dist_dls[n]).cuda()
+                    running_means[f"mean_{n}_gram_kl_div"] += (
+                        kl_divergence_from_logits(ngram_logprobs, logits).mean().item()
+                        / num_iters
+                    )
+                    running_means[f"mean_{n}_gram_js_div"] += (
+                        js_divergence_from_logits(ngram_logprobs, logits).mean().item()
+                        / num_iters
+                    )
+                    del ngram_logprobs
+
+                if loss:
+                    ngram_tokens = (
+                        next(ngram_token_dls[n]).cuda().reshape(batch_size, seq_len)
+                    )
+                    logits = model(ngram_tokens).logits[:, :, :d_vocab]
+                    mean_loss = F.cross_entropy(
+                        logits[:, :-1].flatten(0, 1),
+                        ngram_tokens[:, 1:].flatten(0, 1),
+                        reduction="mean",
+                    ).item()
+                    running_means[f"mean_{n}_gram_loss"] += mean_loss / num_iters
+
+            pbar.update(1)
+        print(running_means[f"mean_{n}_gram_kl_div"])
+        print(running_means[f"mean_{n}_gram_js_div"])
+        print(running_means[f"mean_{n}_gram_loss"])
+
+        for key in running_means.keys():
+            data[key].append(running_means[key])
+
+    # Convert defaultdict to dict for DataFrame
+    return {key: value for (key, value) in data.items()}
+
+
+def get_missing_steps(
+    df: pd.DataFrame,
+    ngram_orders: list[int],
+    steps: list[int],
+    loss: bool,
+    div: bool,
+) -> set[int]:
+    missing_steps = set()
+    for ngram_order in ngram_orders:
+        if loss:
+            if f"mean_{ngram_order}_gram_loss" not in df.columns:
+                missing_steps.update(steps)
+            elif f"mean_{ngram_order}_gram_loss" in df:
+                missing_steps.update(
+                    df[df[f"mean_{ngram_order}_gram_loss"].isnull()]["step"].tolist()
+                )
+        if div:
+            if f"mean_{ngram_order}_gram_kl_div" not in df.columns:
+                missing_steps.update(steps)
+            elif f"mean_{ngram_order}_gram_kl_div" in df:
+                missing_steps.update(
+                    df[df[f"mean_{ngram_order}_gram_kl_div"].isnull()]["step"].tolist()
+                )
+
+    return missing_steps
+
+
+def main(
+    ngram_path: Path,
+    val_path: Path,
+    output_path: Path,
+    ngram_orders: list[int],
+    steps: list[int],
+    output_prefix: str,
+    div: bool,
+    loss: bool,
+    overwrite: bool,
+):
+    output_path.mkdir(exist_ok=True, parents=True)
+
+    mp.set_start_method("spawn")
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+    num_samples = 1024
+    seq_len = 2049
+
+    for model_name, batch_size in (
+        [
+            ("pythia-14m", 8),
+            ("pythia-70m", 4),
+            ("pythia-160m", 4),
+            ("pythia-410m", 4),
+            ("pythia-1b", 4),
+            ("pythia-1.4b", 4),
+            ("pythia-2.8b", 2),
+            ("pythia-6.9b", 1),
+            ("pythia-12b", 1),
+            ("pythia-14m-warmup01", 8),
+            ("pythia-70m-warmup01", 4),
+        ]
+        + [(f"pythia-14m-seed{i}", 16) for i in range(1, 10)]
+        + [(f"pythia-70m-seed{i}", 4) for i in range(1, 10)]
+        + [(f"pythia-160m-seed{i}", 8) for i in range(1, 10)]
+        + [(f"pythia-410m-seed{i}", 4) for i in range(1, 5)]
+    ):
+        print("Collecting data for", model_name)
+
+        current_df_path = output_path / f"ngram_{model_name}_{num_samples}.csv"
+        if not current_df_path.exists():
+            pd.DataFrame({"steps": steps}).to_csv(current_df_path)
+        current_df = pd.read_csv(current_df_path)
+
+        if not overwrite:
+            missing_steps = get_missing_steps(
+                current_df, ngram_orders, steps, loss, div
+            )
+            if not missing_steps:
+                continue
+
+            steps = [int(step) for step in list(missing_steps)]
+
+        debug = True
+        if debug:
+            worker(
+                0,
+                steps,
+                model_name,
+                batch_size,
+                num_samples,
+                len(tokenizer),
+                seq_len,
+                ngram_orders,
+                ngram_path,
+                val_path,
+                div,
+                loss,
+            )
+        else:
+            data = run_workers(
+                worker,
+                # roughly log spaced steps + final step
+                steps,
+                model_name=model_name,
+                batch_size=batch_size,
+                num_samples=num_samples,
+                d_vocab=len(tokenizer.vocab),
+                seq_len=seq_len,
+                ngram_orders=ngram_orders,
+                ngram_path=ngram_path,
+                val_path=val_path,
+                div=div,
+                loss=loss,
+            )
+        df = pd.concat([pd.DataFrame(d) for d in data])
+
+        current_df.to_csv(
+            output_path
+            / "backup"
+            / f"{output_prefix}_{model_name}_{num_samples}_{time.time()}.csv"
+        )
+        df.set_index("step", inplace=True)
+        current_df.set_index("step", inplace=True)
+
+        for col in df.columns:
+            current_df[col] = (
+                df[col].combine_first(current_df[col]) if col in current_df else df[col]
+            )
+
+        current_df.reset_index(inplace=True)
+        current_df.to_csv(
+            output_path / f"{output_prefix}_{model_name}_{num_samples}.csv",
+            index=False,
+        )
+
+
+def run_workers(worker: Callable, steps: list[int], **kwargs) -> list[dict]:
+    """Parallelise inference over model checkpoints."""
+    max_steps_per_chunk = math.ceil(len(steps) / torch.cuda.device_count())
+    step_indices = [
+        steps[i : i + max_steps_per_chunk]
+        for i in range(0, len(steps), max_steps_per_chunk)
+    ]
+
+    args = [
+        (
+            i,
+            step_indices[i],
+            *kwargs.values(),
+        )
+        for i in range(len(step_indices))
+    ]
+    print(f"Inference on steps {step_indices}, GPU count: {len(step_indices)}")
+    with mp.Pool(len(step_indices)) as pool:
+        return pool.starmap(worker, args)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--n",
+        default=[3],
+        nargs="+",
+        type=int,
+        help="Which n-gram orders to collect data for",
+    )
+    parser.add_argument(
+        "--loss",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--div",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--output_prefix",
+        "-p",
+        default="ngram",
+        help="CSV name prefix",
+        type=str,
+    )
+    parser.add_argument(
+        "--ngram_path",
+        default="/mnt/ssd-1/lucia/ngrams-across-time/data",
+        help="Path to n-gram data",
+    )
+    parser.add_argument(
+        "--val_path",
+        default="data/pile-deduped/val_tokenized.hf",
+        help="Path to validation data",
+    )
+    parser.add_argument(
+        "--output_path",
+        default="output",
+        help="Path to save output CSVs",
+    )
+    parser.add_argument(
+        "--steps",
+        default=[
+            1,
+            2,
+            4,
+            8,
+            16,
+            32,
+            64,
+            128,
+            256,
+            512,
+            1000,
+            2000,
+            5000,
+            8000,
+            16_000,
+            33_000,
+            66_000,
+            131_000,
+            143_000,
+        ],
+        nargs="+",
+        help="Which model checkpoints to collect data for",
+    )
+    args = parser.parse_args()
+    main(
+        Path(args.ngram_path),
+        Path(args.val_path),
+        Path(args.output_path),
+        args.n,
+        args.steps,
+        args.output_prefix,
+        args.div,
+        args.loss,
+        args.overwrite,
+    )
--- scripts/script_utils/divergences.py
+++ scripts/script_utils/divergences.py
@@ -3,32 +3,32 @@ import math
 import torch
 
 
-def kl_divergence_log_space(
-    logit_p: torch.Tensor, logit_q: torch.Tensor, dim: int = -1
+def kl_divergence_from_logits(
+    p: torch.Tensor, q: torch.Tensor, dim: int = -1
 ) -> torch.Tensor:
-    """Compute the KL divergence between two sets of logits."""
-    logsumexp_p = logit_p.logsumexp(dim).unsqueeze(dim)
-    logsumexp_q = logit_q.logsumexp(dim).unsqueeze(dim)
+    """Memory-efficient KL divergence between two sets of logits.
+    Intermediate variables are (p.size(0), 1).
+
+    Note that p.sub(logsumexp_p) is equivalent to converting p
+    to log probabilities."""
+    logsumexp_p = p.logsumexp(dim).unsqueeze(dim)
+    logsumexp_q = q.logsumexp(dim).unsqueeze(dim)
 
     return torch.nansum(
-        logit_p.sub(logsumexp_p).exp()
-        * (logit_p.sub(logsumexp_p) - logit_q.sub(logsumexp_q)),
+        p.sub(logsumexp_p).exp() * (p.sub(logsumexp_p) - q.sub(logsumexp_q)),
         dim,
     )
 
 
-def kl_divergence_linear_space(
-    probs_p: torch.Tensor, probs_q: torch.Tensor, dim: int = -1
+def kl_divergence_from_probabilities(
+    p: torch.Tensor, q: torch.Tensor, dim: int = -1
 ) -> torch.Tensor:
-    """Compute the KL divergence between two sets of logits."""
+    """Compute the KL divergence between two sets of probabilities."""
 
-    return torch.nansum(
-        (probs_p / probs_q).log() * probs_p,
-        dim,
-    )
+    return torch.nansum(p * (p / q).log(), dim)
 
 
-def js_divergence_log_space(
+def js_divergence_from_logits(
     logit_p: torch.Tensor, logit_q: torch.Tensor, dim: int = -1
 ) -> torch.Tensor:
     """Compute the Jensen-Shannon divergence between two sets of logits"""
@@ -54,6 +54,8 @@ def js_divergence_log_space(
 def one_hot_js_divergence(
     logit_q: torch.Tensor, p_index: torch.Tensor, batch: int, dim: int = -1
 ) -> torch.Tensor:
+    """Compute the Jensen-Shannon divergence between
+    a one-hot vector and a set of logits."""
     logsumexp_q = logit_q.logsumexp(-1, keepdim=True)
 
     # accumulate log_m (starts in linear space)
